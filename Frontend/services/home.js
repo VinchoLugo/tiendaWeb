a28607fc07e1ad7c1849f87693efd5e6c6d0c811
Voltaire321 <cesarale012@gmail.com>
2024-05-27 04:16:07 -0700

    este es el bueno2

--- Frontend/services/home.js
+++ Frontend/services/home.js
@@ -1,6 +1,8 @@
+let allGames = [];
+
 async function fetchGameData(gameid) {
     try {
-        const response = await fetch(`http://127.0.0.1:3000/steam/${gameid}`);
+        const response = await fetch(`/steam/${gameid}`);
         const data = await response.json();
         return data;
     } catch (error) {
@@ -10,48 +12,161 @@ async function fetchGameData(gameid) {
 
 async function fillGameData() {
     try {
-        const response = await fetch('/Frontend/src/catalog.json');
+        const response = await fetch('/src/catalog.json');
         const games = await response.json();
 
         const container = document.querySelector('#juegos-lista');
+        container.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevas tarjetas
 
         games.forEach(async game => {
             const gameData = await fetchGameData(game.appid);
             if (gameData && gameData[game.appid] && gameData[game.appid].data) {
                 const gameInfo = gameData[game.appid].data;
-        
-                const card = document.createElement('div');
-                card.classList.add('card');
-                card.setAttribute('data-appid', game.appid); // Almacena el appid en el atributo data-appid
-        
+
+                const cardmamalon = document.createElement('div');
+                cardmamalon.classList.add('cardmamalon');
+                cardmamalon.setAttribute('data-appid', game.appid);
+
                 const img = document.createElement('img');
                 img.src = gameInfo.header_image;
                 img.alt = gameInfo.name;
-        
+
                 const title = document.createElement('h3');
+                title.classList.add('textCard');
                 title.textContent = gameInfo.name;
-        
+
                 const price = document.createElement('p');
-                price.textContent = gameInfo.price_overview.final_formatted;
-        
-                card.appendChild(img);
-                card.appendChild(title);
-                card.appendChild(price); // Agrega el precio a la tarjeta
-        
-                container.appendChild(card);
-        
-                // Agrega un evento click a la tarjeta para redirigir a gameinfo.html con el appid
-                card.addEventListener('click', () => {
-                    window.location.href = `/Frontend/pages/gameinfo.html?appid=${game.appid}`;
+                if (gameInfo.price_overview && gameInfo.price_overview.final_formatted) {
+                    price.textContent = gameInfo.price_overview.final_formatted;
+                } else {
+                    price.textContent = 'Precio no disponible';
+                }
+
+                cardmamalon.appendChild(img);
+                cardmamalon.appendChild(title);
+                cardmamalon.appendChild(price);
+
+                container.appendChild(cardmamalon);
+
+                cardmamalon.addEventListener('click', () => {
+                    window.location.href = `/pages/gameinfo.html?appid=${gameInfo.steam_appid}`;
                 });
             }
         });
-        
+
     } catch (error) {
         console.error('Error al llenar los datos del juego:', error);
     }
 }
 
-window.onload = function() {
-    fillGameData();
+async function fetchAndStoreGameData() {
+    try {
+        const response = await fetch('/src/catalog.json');
+        const games = await response.json();
+
+        allGames = games;
+        fillGameData();
+
+    } catch (error) {
+        console.error('Error al obtener los datos del juego:', error);
+    }
+}
+
+function filterGamesByGenre(genre) {
+    const container = document.querySelector('#juegos-lista');
+    container.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevas tarjetas
+
+    allGames.forEach(async game => {
+        if (game.genre.includes(genre)) {
+            const gameData = await fetchGameData(game.appid);
+            if (gameData && gameData[game.appid] && gameData[game.appid].data) {
+                const gameInfo = gameData[game.appid].data;
+
+                const cardmamalon = document.createElement('div');
+                cardmamalon.classList.add('cardmamalon');
+                cardmamalon.setAttribute('data-appid', game.appid);
+
+                const img = document.createElement('img');
+                img.src = gameInfo.header_image;
+                img.alt = gameInfo.name;
+
+                const title = document.createElement('h3');
+                title.classList.add('textCard');
+                title.textContent = gameInfo.name;
+
+                const price = document.createElement('p');
+                if (gameInfo.price_overview && gameInfo.price_overview.final_formatted) {
+                    price.textContent = gameInfo.price_overview.final_formatted;
+                } else {
+                    price.textContent = 'Precio no disponible';
+                }
+
+                cardmamalon.appendChild(img);
+                cardmamalon.appendChild(title);
+                cardmamalon.appendChild(price);
+
+                container.appendChild(cardmamalon);
+
+                cardmamalon.addEventListener('click', () => {
+                    window.location.href = `/pages/gameinfo.html?appid=${gameInfo.steam_appid}`;
+                });
+            }
+        }
+    });
+}
+
+document.querySelectorAll('.genres button').forEach(button => {
+    button.addEventListener('click', (event) => {
+        const genre = event.target.textContent;
+        filterGamesByGenre(genre);
+    });
+});
+
+const createCarouselItem = (content, isActive) => {
+    const div = document.createElement('div');
+    div.className = 'carousel-item' + (isActive ? ' active' : '');
+    const itemContainer = document.createElement('div');
+    itemContainer.className = 'd-flex';
+    itemContainer.appendChild(content);
+    div.appendChild(itemContainer);
+    return div;
 };
+
+async function createCarousel() {
+    try {
+        const response = await fetch('/src/catalog.json');
+        const games = await response.json();
+
+        const carouselInner = document.querySelector('#carousel-inner');
+        carouselInner.innerHTML = ''; // Limpiar el carrusel antes de agregar nuevas imágenes
+
+        games.forEach(async (game, index) => {
+            const gameData = await fetchGameData(game.appid);
+            if (gameData && gameData[game.appid] && gameData[game.appid].data) {
+                const gameInfo = gameData[game.appid].data;
+
+                const img = document.createElement('img');
+                img.src = gameInfo.header_image;
+                img.alt = gameInfo.name;
+                img.className = 'd-block w-100';
+
+                const carouselItem = createCarouselItem(img, index === 0); // La primera imagen es activa
+                carouselInner.appendChild(carouselItem);
+
+                img.addEventListener('click', () => {
+                    window.location.href = `/pages/gameinfo.html?appid=${gameInfo.steam_appid}`;
+                });
+            }
+        });
+
+    } catch (error) {
+        console.error('Error al crear el carrusel:', error);
+    }
+}
+
+
+window.onload = function() {
+    fetchAndStoreGameData();
+    createCarousel();
+};
+
